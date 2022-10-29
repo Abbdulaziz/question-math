ccb1b10411fbe5d769ea72ef70658fd517820f9f
abbdulaziz <tabdulaziz313@gmail.com>
2022-10-29 20:54:20 +0500

    add mats

---
+++ main.js
@@ -0,0 +1,85 @@
+// 1-misol
+// let a = 5;
+// for(let i= 0; i<10; i++){
+//     console.log( a);
+// }
+// 2-misol
+
+// let a = 1;
+// for(let i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+// 3-misol
+
+// for (let i=10 ;i>0 ;i--){
+//     console.log(i)
+// }
+
+// 4-misol
+// let a = 10000;
+// for(let i = 1; i<=10; i++){
+//     console.log(i*a);
+// }
+// 5-misol
+// let a = 1000;
+// for(let i = 1; i<=10; i++){
+//     console.log(i*a);
+// }
+
+// 6-misol
+
+// let a =1000;
+// for(let i = 10; i<=20; i++){
+//     if( i%2==0){
+//         console.log(i*a);
+//     }
+// }
+
+// 7-misol
+// let b = 0;
+// for(let i =0; i<=3; i++){
+//     b = i+b;
+// }
+// console.log(b);
+
+
+// 14-misol
+
+// let a = 0;
+
+// for (let i = 0; i <10; i++) {
+//     if(i%2==0){
+
+//     }
+// }
+
+
+
+// let a=2;
+// let b =2;
+// let q = 3;
+
+// if( a/a==1 && a%2==0 || a%3==0  ||a==b &&a==q{
+
+//     console.log('tub son emas');
+// }else{
+//     console.log(a);
+// }
+
+
+let b = 454;
+let son = 100;
+for (i = 1; i <= son; i++) {
+    if (son % i == 0) {
+        b++;
+    }
+
+}
+if (b >= 3) {
+    console.log("Murakkab son");
+
+}
+else {
+    console.log("Murakkab son emas");
+}
