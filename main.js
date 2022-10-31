761d8f078548aa9561006c98cee97ef176656503
abbdulaziz <tabdulaziz313@gmail.com>
2022-10-31 07:18:13 +0500

    add question

--- main.js
+++ main.js
@@ -38,48 +38,116 @@
 
 // 7-misol
 // let b = 0;
-// for(let i =0; i<=3; i++){
+// for(let i =0; i<=10; i++){
 //     b = i+b;
 // }
 // console.log(b);
 
+// 8-misol
+// let b = 1;
+// for (let i = 1; i <= 10; i++) {
+//     b = b * i;
+
+// }
+// console.log(b);
+
+
+// 9-misol
+
+// let b = 1;
+// for (let i = 1; i <= 10; i++) {
+//     b = (i * i)+b ;
+
+// }
+// console.log(b);
+
+// 10-misol
+// let j = 0;
+// let q = 0;
+// for (let i = 1; i <= 10; i++) {
+//     q = 1 / i;
+//     j = j + q;
+//     console.log(q);
+// }
+// console.log(j);
+// 11-misol
+// for(let i=0; i<=10;i++){
+//     if(i%2==1){
+//         console.log(i);
+//     }
+// }
+// 12-misol
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+// 13-misol
+
+
+// var primos = [];
+// for (var i = 0; i <= 100; i++) {
+//     var fake = true;
+//     for (var j = 2; j <= i; j++) {
+//         if (i%j==0 && j!=i) {
+//             fake =false;
+//         }
+//     }
+//     if (fake === true) {
+//         // primos.push(i);
+//         console.log(i);
+//     }
+// }
+// // primos = primos.reverse();
+// for(i = primos.length; i > 0; i--) {
+//     console.log(primos[i]);
+// }
+
 
 // 14-misol
 
-// let a = 0;
 
-// for (let i = 0; i <10; i++) {
-//     if(i%2==0){
-
+// for (var i = 0; i <= 100; i++) {
+//     var fake = true;
+//     for (var j = 2; j <= i; j++) {
+//         if (i%j==0 && j!=i) {
+//             fake =false;
+//         }
+//     }
+//     if (fake === false) {
+//         console.log(i);
 //     }
 // }
 
+// 15-misol
 
-
-// let a=2;
-// let b =2;
-// let q = 3;
-
-// if( a/a==1 && a%2==0 || a%3==0  ||a==b &&a==q{
-
-//     console.log('tub son emas');
-// }else{
-//     console.log(a);
+// for(let i=0; i<=100; i++){
+//     console.log(i*i);
 // }
 
+// 16-misol
 
-let b = 454;
-let son = 100;
-for (i = 1; i <= son; i++) {
-    if (son % i == 0) {
-        b++;
-    }
 
-}
-if (b >= 3) {
-    console.log("Murakkab son");
+// for(let i=100; i>0;i--){
+//     console.log(i);
+// }
 
-}
-else {
-    console.log("Murakkab son emas");
-}
+// 17-misol
+
+// for(i=0; i<=100; i++){
+//     let b =false;
+//     if(i%5==0){
+//         b=true;
+//     }else{
+//         console.log(i);
+//     }
+// }
+
+// 18-misol
+// let b=0;
+// for(i=0; i<=100; i++){
+//     b=(i*i)+b;
+
+// }
+// console.log(b);
